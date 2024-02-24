1b8da8dac78c73b2202609d6400bc9f45c656e4d
ilkay <06ilkayozturk@gmail.com>
2024-02-24 13:31:08 +0300

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,6 +8,7 @@ import {
   signout,
   test,
   updateUser,
+  getUsers,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -17,5 +18,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
