a5a8e17e4663e1480dec89b086cad4d382c6eb26
ilkay <06ilkayozturk@gmail.com>
2024-02-25 22:20:45 +0300

    show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,6 +9,7 @@ import {
   test,
   updateUser,
   getUsers,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -19,5 +20,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
