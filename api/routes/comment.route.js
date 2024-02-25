33bb6d79f956174dc31214d8196c92524980145a
ilkay <06ilkayozturk@gmail.com>
2024-02-25 14:07:36 +0300

    add comment section to the post page

---
+++ api/routes/comment.route.js
@@ -0,0 +1,13 @@
+"use strict";
+/* --------------------------------------
+              | MERN-BLOG |
+----------------------------------------- */
+import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import { createComment } from "../controllers/comment.controller.js";
+
+const router = express.Router();
+
+router.post("/create", verifyToken, createComment);
+
+export default router;
