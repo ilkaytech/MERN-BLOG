126430f46aed744d67dd37d7b18b6eeb69a502c1
ilkay <06ilkayozturk@gmail.com>
2024-02-26 22:32:54 +0300

    add comments section to the admin dashboard

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -3,7 +3,6 @@
               | MERN-BLOG |
 ----------------------------------------- */
 import Comment from "../models/comment.model.js";
-import { errorHandler } from "../utils/error.js";
 
 export const createComment = async (req, res, next) => {
   try {
@@ -71,6 +70,7 @@ export const editComment = async (req, res, next) => {
         errorHandler(403, "You are not allowed to edit this comment")
       );
     }
+
     const editedComment = await Comment.findByIdAndUpdate(
       req.params.commentId,
       {
@@ -97,6 +97,33 @@ export const deleteComment = async (req, res, next) => {
     }
     await Comment.findByIdAndDelete(req.params.commentId);
     res.status(200).json("Comment has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getcomments = async (req, res, next) => {
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not allowed to get all comments"));
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "desc" ? -1 : 1;
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
   } catch (error) {
     next(error);
   }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -6,10 +6,11 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
-  getPostComments,
-  likeComment,
-  editComment,
   deleteComment,
+  editComment,
+  getPostComments,
+  getcomments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
@@ -19,5 +20,6 @@ router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 router.put("/editComment/:commentId", verifyToken, editComment);
 router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
+router.get("/getcomments", verifyToken, getcomments);
 
 export default router;
